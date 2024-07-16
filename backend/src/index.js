aa8e16f9b3c23e9d7a808ca5edd025bb30f51e8f
XxJuan23xX <manuelsima0305@gmail.com>
2024-07-16 13:30:10 -0600

    Cambios realizados

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,10 +12,12 @@ app.use(cors());
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api/', orderRoutes);
+app.use('/api/orders', orderRoutes); // Asegúrate de que esta ruta sea correcta
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app; // Asegúrate de exportar la aplicación
