8619facfe634f6cc8d0b9cdabaf5dfbf31455670
XxJuan23xX <manuelsima0305@gmail.com>
2024-07-16 16:18:08 -0600

    Cambios realizados

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes); // Asegúrate de que esta ruta sea correcta
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
